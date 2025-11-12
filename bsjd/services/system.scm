8ef165f35096659fbd4d51596e2384062fc76783
Bart Demeyer <bart.demeyer@gmail.com>
2025-11-12 19:17:04 +0100

    Added Jetbrains utility

--- bsjd/services/system.scm
+++ bsjd/services/system.scm
@@ -1,33 +1,61 @@
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;; Copyright © 2024 Giacomo Leidi <therewasa@fishinthecalculator.me>
+
 (define-module (bsjd services system)
-  #:use-module (guix gexp)
-  #:use-module (guix records)
   #:use-module (gnu services)
-  #:use-module (gnu services admin)
+  #:use-module (gnu services configuration)
   #:use-module (gnu services dbus)
   #:use-module (gnu services shepherd)
-  #:use-module (gnu packages firmware))
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (srfi srfi-1)
+  #:use-module (nongnu packages firmware)
+  #:export (fwupd-configuration
+            fwupd-configuration?
+            fwupd-configuration-fields
+            fwupd-configuration-fwupd
+            fwupd-service-type))
 
-(define-record-type* <fwupd-configuration>
-  fwupd-configuration make-fwupd-configuration
-  fwupd-configuration?
-  (fwupd fwupd-configuration-fwupd
-         (default fwupd)))
+(define-configuration/no-serialization fwupd-configuration
+  (fwupd
+    (package fwupd)
+    "The fwupd package that will be installed in the system profile."))
 
 (define (fwupd-activation config)
-    (with-imported-modules '((guix build utils))
-      #~(begin
-          (use-modules (guix build utils))
-          (mkdir-p "/var/cache/fwupd")
-          (mkdir-p "/var/lib/fwupd/local.d")
-          (mkdir-p "/var/lib/fwupd/"))))
+  (with-imported-modules '((guix build utils))
+    #~(begin
+        (use-modules (guix build utils))
+        (mkdir-p "/var/cache/fwupd")
+        (mkdir-p "/var/lib/fwupd/local.d")
+        (mkdir-p "/var/lib/fwupd/"))))
 
-(define-public fwupd-service-type
-  (let ((fwupd-package (compose list fwupd-configuration-fwupd)))
-    (service-type
-     (name 'fwupd)
-     (extensions
-      (list (service-extension polkit-service-type fwupd-package)
-	    (service-extension dbus-root-service-type fwupd-package)
-            (service-extension activation-service-type fwupd-activation)))
-     (default-value (fwupd-configuration))
-     (description "A service that runs the necessary daemons for fwupd."))))
+(define (fwupd-shepherd-services config)
+  (let ((fwupd
+        (fwupd-configuration-fwupd config)))
+    (list
+      (shepherd-service (provision '(fwupd))
+                        (requirement '(user-processes))
+                        (documentation "Run the fwupd daemon.")
+                        (start
+                          #~(make-forkexec-constructor
+                              (list #$(file-append fwupd "/libexec/fwupd/fwupd"))))
+                        (stop
+                          #~(make-kill-destructor))))))
+
+(define fwupd-service-type
+  (service-type (name 'fwupd)
+                (extensions
+                  (list
+                    (service-extension shepherd-root-service-type
+                                      fwupd-shepherd-services)
+                    (service-extension profile-service-type
+                                      (compose list fwupd-configuration-fwupd))
+                    (service-extension dbus-root-service-type
+                                      (compose list fwupd-configuration-fwupd))
+                    (service-extension polkit-service-type
+                                      (compose list fwupd-configuration-fwupd))
+                    (service-extension activation-service-type
+                                      (compose list fwupd-activation))))
+                (default-value (fwupd-configuration))
+                (description "This service configures fwupd on the Guix System.")))
+
